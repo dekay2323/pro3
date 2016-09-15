bcc4fb0fc26ca13691084bab94462602cf7fd5e1
DemianK <demiankrige@gmail.com>
2016-09-14 20:54:31 -0600

    DB: Start db from scratch

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -6,22 +6,24 @@ import com.pro3.user.UserRole
 class BootStrap {
 
     def init = { servletContext ->
-        def strategy = Strategy.findOrSaveByName('Sole Source')
+        Strategy.findOrSaveByName('Sole Source')
         Strategy.findOrSaveByName('Competitive Bid')
-        def client = Client.findOrSaveByName('ACME Oilfield Ltd')
-        Client.findOrSaveByName('Charlie Construction')
-        def leadTime = LeadTime.findOrSaveByName('ARO')
+
+        LeadTime.findOrSaveByName('ARO')
         LeadTime.findOrSaveByName('ARD')
-        def status = RequestStatus.findOrSaveByName('Add to Plan')
+
+        RequestStatus.findOrSaveByName('Add to Plan')
         RequestStatus.findOrSaveByName('Start')
         RequestStatus.findOrSaveByName('Approved to Plan')
         RequestStatus.findOrSaveByName('RFQ Issued')
         RequestStatus.findOrSaveByName('Bids Recieved')
         RequestStatus.findOrSaveByName('Evaluation Complete')
         RequestStatus.findOrSaveByName('PO Issued')
-        def vendor = Vendor.findOrSaveByName('Joe\'s Flooring')
+
+        Vendor.findOrSaveByName('Joe\'s Flooring')
         Vendor.findOrSaveByName('Demian\'s Hardwood')
-        def wbs = Wbs.findOrSaveByCode('105.1')
+
+        Wbs.findOrSaveByCode('105.1')
         Wbs.findOrSaveByCode('105.2')
         Wbs.findOrSaveByCode('106.1')
 
