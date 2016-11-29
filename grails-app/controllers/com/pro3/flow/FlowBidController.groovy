5c4321a0d5903e484aa0ff902a96d5f84b04a134
Demian Krige <demian@stratahealth.com>
2016-11-28 21:54:27 -0700

    cleanup imports

--- grails-app/controllers/com/pro3/flow/FlowBidController.groovy
+++ grails-app/controllers/com/pro3/flow/FlowBidController.groovy
@@ -1,16 +1,12 @@
 package com.pro3.flow
 
-import com.pro3.Client
-import com.pro3.Quote
 import com.pro3.Rfq
-import com.pro3.user.User
 import grails.plugin.springsecurity.annotation.Secured
 import grails.transaction.Transactional
 
 @Secured(['ROLE_ADMIN', 'ROLE_USER'])
 @Transactional(readOnly = true)
 class FlowBidController {
-    def authUserService
 
     def index() {
         log.debug("flowBid() ${params}")
