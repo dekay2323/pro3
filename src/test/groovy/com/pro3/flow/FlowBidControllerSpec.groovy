5daedef94f5b8d67e4dd70953db02624d3c1d22e
Demian Krige <demian@stratahealth.com>
2016-11-28 21:53:46 -0700

    TEST: FlowBidController tests

---
+++ src/test/groovy/com/pro3/flow/FlowBidControllerSpec.groovy
@@ -0,0 +1,43 @@
+package com.pro3.flow
+
+import com.pro3.Rfq
+import grails.buildtestdata.mixin.Build
+import grails.test.mixin.TestFor
+import spock.lang.*
+
+import javax.servlet.http.HttpServletResponse
+
+@TestFor(FlowBidController)
+@Build(Rfq)
+class FlowBidControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test index cannot find"() {
+        when:
+        params.id = '1'
+        controller.index()
+
+        then:
+        response.getStatus() == HttpServletResponse.SC_NOT_FOUND
+    }
+
+    void "test index"() {
+        setup:
+        Rfq rfq = Rfq.build(name: 'rfqName')
+
+        when:
+        params.id = rfq.id
+        controller.index()
+
+        then:
+        response.getStatus() == HttpServletResponse.SC_OK
+        model?.rfq != null
+        model?.rfq?.id == 1
+        model?.rfq?.name == 'rfqName'
+    }
+}
