1edf034936ea3fe8a54a6b0312846b8721c78645
Demian Krige <demian@stratahealth.com>
2016-12-06 21:14:20 -0700

    FILES: Get interface to work and tested

--- grails-app/services/com/pro3/AmazonService.groovy
+++ grails-app/services/com/pro3/AmazonService.groovy
@@ -1,22 +1,23 @@
 package com.pro3
 
+import com.amazonaws.services.s3.model.ObjectListing
+import com.amazonaws.services.s3.model.S3ObjectSummary
 import grails.plugin.awssdk.s3.AmazonS3Service
-import grails.transaction.Transactional
-
-import javax.annotation.PostConstruct
 
 class AmazonService extends AmazonS3Service {
+    final String BUCKET_NAME = 'p3app'
 
-    @PostConstruct
-    def init() {
-        init('p3app')
+    def listFilesForAccount(String accountName) {
+        assert accountName
+        ObjectListing objectListing = this.listObjects(BUCKET_NAME, "${accountName}/")
+        objectListing?.getObjectSummaries().collect { S3ObjectSummary obj ->
+            obj.key
+        }
     }
     
-    def listAllBuckets() {
-        this.listBucketNames()
-    }
-    
-    def listFiles() {
-        this.listObjects()
+    String storeFileForAccount(String accountName, File file) {
+        assert file
+        assert accountName
+        this.storeFile(BUCKET_NAME, "${accountName}/${file.name}", file)
     }
 }
--- src/integration-test/groovy/com/pro3/AmazonServiceSpec.groovy
+++ src/integration-test/groovy/com/pro3/AmazonServiceSpec.groovy
@@ -1,5 +1,6 @@
 package com.pro3
 
+import com.amazonaws.services.s3.model.ObjectListing
 import grails.test.mixin.integration.Integration
 import grails.transaction.*
 
@@ -19,13 +20,24 @@ class AmazonServiceSpec extends GebSpec {
     def cleanup() {
     }
 
-    def "testing p3app bucket can be found"() {
-        expect:
-        true == amazonService.listAllBuckets().size() >= 1
-    }
-    
-    def "testing p3app bucket has some files"() {
-        expect:
-        true == amazonService.listFiles().size() > 0
+    def "testing create file"() {
+        setup:
+        File file = new File("test-file.txt")
+        file << "Test upload\n"
+        String account = 'test-account'
+        
+        when:
+        String url = amazonService.storeFileForAccount(account, file)
+        
+        then:
+        url == "https://s3-us-west-2.amazonaws.com/p3app/test-account/test-file.txt"
+        
+        when:
+        def list = amazonService.listFilesForAccount(account)
+        
+        then:
+        list.size() == 2
+        list[0] == 'test-account/'
+        list[1] == 'test-account/test-file.txt'
     }
 }
