0349dc0e7341f72d06df368f11876ccbc0f12fb5
Demian Krige <demian@demian.hitronhub.home>
2017-02-12 16:10:05 -0700

    BUG: Project number must be unique

--- grails-app/domain/com/pro3/Project.groovy
+++ grails-app/domain/com/pro3/Project.groovy
@@ -26,7 +26,7 @@ class Project {
     static transients = ['budget', 'committed', 'accrued', 'incurred']
 
     static constraints = {
-        projectNumber nullable: false, size: 0..25
+        projectNumber nullable: false, unique: true, size: 0..25
         name nullable: false, size: 0..50
         shortDescription nullable: true, size: 0..50
         client nullable: false
