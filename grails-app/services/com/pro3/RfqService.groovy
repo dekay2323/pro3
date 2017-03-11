147e0e272fb3e370cfde10d396f330ac93b208b0
Demian Krige <demian@demian.hitronhub.home>
2017-03-11 14:32:43 -0700

    Quotes: Needs changed by

--- grails-app/services/com/pro3/RfqService.groovy
+++ grails-app/services/com/pro3/RfqService.groovy
@@ -1,24 +1,25 @@
 package com.pro3
 
+import com.pro3.user.User
 import grails.transaction.Transactional
 import grails.validation.ValidationException
 import org.springframework.validation.Errors
 
 @Transactional
 class RfqService {
-
+    
     // @TODO why do we have to pass quotestatus in
     @Transactional
-    def Rfq createRfqAndQuotes(MaterialRequest materialRequest, QuoteStatus quoteStatus) {
+    def Rfq createRfqAndQuotes(User user, MaterialRequest materialRequest, QuoteStatus quoteStatus) {
         log.debug("createRfqs() ${materialRequest}")
 
         Rfq rfq = new Rfq(materialRequest: materialRequest)
         if (!materialRequest?.bidders) {
             throw new Pro3Exception('No bidders')
         }
-
+        
         materialRequest?.bidders?.each {vendor->
-            Quote quote = new Quote(rfq: rfq, vendor: vendor, status: quoteStatus)
+            Quote quote = new Quote(rfq: rfq, vendor: vendor, status: quoteStatus, changedBy: user)
             rfq.name = "${materialRequest?.reqNumber} - ${materialRequest?.description}"
             rfq.addToQuotes(quote)
             rfq.save failOnError: true
